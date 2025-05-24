8ad1694fb6454dadedd1d9a558d89d1e31c3dfbc
vprabhu1771 <vprabhu1771@gmail.com>
2025-05-24 12:12:07 +0530

    Create IsUser.php
    php artisan make:middleware IsUser

---
+++ app/Http/Middleware/IsUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Illuminate\Support\Facades\Auth;
+
+class IsUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->isUser()) {
+            return $next($request);
+        }
+
+        abort(403, 'Unauthorized: Users only.');
+    }
+}
